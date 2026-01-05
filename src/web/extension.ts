2da755cca7f5c19bbbd31c5d0910df4bd29a78a2
Albresky <albre02@outlook.com>
2026-01-05 09:04:00 +0000

    Add dummy src

---
+++ src/web/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    // No activation needed for syntax highlighting
+}
+
+export function deactivate() {}
